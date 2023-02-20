ba3a82e547003b50e119926867a14873a9a8ae09
jillian <jillianl2025@student.cis.edu.hk>
2023-02-20 09:44:57 +0800

    complete

--- src/Scanners.java
+++ src/Scanners.java
@@ -2,29 +2,41 @@ public class Scanners {
 
     public static void main(String[] args) {
 
-        // 1.
+        // 1
+        Scanner scanner = new Scanner(System.in);
 
-        // 2.
+        // 2
+        System.out.println("Please enter your first name:");
 
-        // 3.
+        // 3
+        String firstName = scanner.nextLine();
 
-        // 4.
+        // 4
+        System.out.println("Please enter your last name:");
 
-        // 5.
+        // 5
+        String lastName = scanner.nextLine();
 
-        // 6.
+        // 6
+        System.out.println("Please enter your age:");
 
-        // 7.
+        // 7
+        int age = scanner.nextInt();
 
-        // 8.
+        // 8
+        System.out.println("Please enter your favourite song:");
 
-        // 9.
+        // 9
+        String favSong = scanner.next();
 
-        // 10.
+        // 10
+        String info = "Hi, my name is " + firstName + " " + lastName + ". I am " + age + " years old and my favourite song is " + favSong + ".";
 
-        // 11.
+        // 11
+        System.out.println(info);
 
-        // 12.
+        // 12
+        scanner.close();
 
     }
 }
